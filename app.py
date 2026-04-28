67cb6bbe8f32f7337858050138eade32a729a354
Brian Chaya <brian.chaya2005@gmail.com>
2026-04-28 13:18:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -413,6 +413,12 @@ def grouping(db):
         if id_count > 1 or kode_count > 1:
             return "DOUBLE"
 
+         kode_words = [w.strip() for w in kode_part.strip().split() if w.strip()]
+        if len(kode_words) >= 5:
+            return "NA"
+        if len(kode_words) == 1 and len(kode_words[0]) <= 2:
+            return "NA"
+
         return "NORMAL"
 
     grouped["TYPE"] = grouped.apply(is_double, axis=1)
